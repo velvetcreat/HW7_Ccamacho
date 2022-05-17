fe2fa2c37f34cfba69ddae4b7b5b525119a2f84b
Velvetcreature <cinemacelery@gmail.com>
2022-05-16 19:57:59 -0700

    update units to work better

--- src/index.js
+++ src/index.js
@@ -39,11 +39,11 @@ let month = months[date.getMonth()];
 let currentDate = document.querySelector("#current-date");
 currentDate.innerHTML = formatDate(now);
 //                    search engine function
-
+ 
 //               Search button activation from form
 let showCurrentCity = document.querySelector("#search-city");
 showCurrentCity.addEventListener("submit", showCity);
-
+ 
 //              connecting axios & Open Weather API &&
 //               New search updating HTML city
 function showCity(event) {
@@ -51,11 +51,11 @@ function showCity(event) {
   let currentCity = document.querySelector("#city-input").value;
   let place = document.querySelector("#current-city");
   place.innerHTML = `${currentCity}`;
-
+ 
   let apiKey = "7a25c6b2ec87adec4dc53604efe82144";
   let units = "metric";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${currentCity}&appid=${apiKey}&units=${units}`;
-
+ 
   axios.get(apiUrl).then(showCurrentTemp);
 }
 //      Update HTML temperature with current temperature from search
@@ -63,23 +63,23 @@ function showCurrentTemp(response) {
   let cityTemp = Math.round(response.data.main.temp);
   let degrees = document.querySelector("#current-temp");
   degrees.innerHTML = `${cityTemp}`;
-
+ 
   let cityDescription = response.data.weather[0].description;
   let description = document.querySelector(".description");
   description.innerHTML = `"Current weather outlook: ${cityDescription}"`;
-
+ 
   let humidityTemp = Math.round(response.data.main.humidity);
   let humidity = document.querySelector("#humidity");
   humidity.innerHTML = `Humidity: ${humidityTemp}%`;
-
+ 
   let windSpeed = Math.round(response.data.wind.speed);
   let wind = document.querySelector("#wind");
   wind.innerHTML = `Wind: ${windSpeed}km/h`;
-
+ 
   let iconElement = document.querySelector("#current-icon");
   iconElement.setAttribute("alt", response.data.weather[0].description);
   iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
-
+ 
   celsiusTemperature= response.data.main.temp;
   temperatureElement.innerHTML= Math.round(celsiusTemperature);
 }
@@ -107,7 +107,7 @@ function convertToCelsius(event) {
 celsiusTemperature = null;
 let celsiusLink = document.querySelector("#cel-link");
 celsiusLink.addEventListener("click", convertToCelsius);
-
+ 
 let fahrenheitLink = document.querySelector("#fahr-link");
 fahrenheitLink.addEventListener("click", convertToFahrenheit);
 //          Current location button & temperature
@@ -123,11 +123,11 @@ function retrievePosition(event) {
 function currentPosition(position) {
   let lat = position.coords.latitude;
   let lon = position.coords.longitude;
-
+ 
   let apiKey = "7a25c6b2ec87adec4dc53604efe82144";
   let apiEndPoint = `https://api.openweathermap.org/data/2.5/weather?`;
-  let apiUrl = `${apiEndPoint}lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
-
+  let apiUrl = `${apiEndPoint}lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+ 
   axios.get(apiUrl).then(showAreaTemp);
 }
 //              4. updating the Temperature HTML
@@ -135,24 +135,32 @@ function showAreaTemp(response) {
   let currentTemp = Math.round(response.data.main.temp);
   let temperature = document.querySelector("#current-temp");
   temperature.innerHTML = `${currentTemp}`;
-
+ 
   let currentCity = response.data.name;
   let place = document.querySelector("#current-city");
   place.innerHTML = currentCity;
-
+ 
   let currentDescription = response.data.weather[0].main;
   let description = document.querySelector(".description");
   description.innerHTML = `"Current weather outlook: ${currentDescription}"`;
-
+ 
   let humidityTemp = Math.round(response.data.main.humidity);
   let humidity = document.querySelector("#humidity");
   humidity.innerHTML = `Humidity: ${humidityTemp}%`;
-
+ 
   let windSpeed = Math.round(response.data.wind.speed);
   let wind = document.querySelector("#wind");
   wind.innerHTML = `Wind: ${windSpeed}km/h`;
-
+ 
   let iconElement = document.querySelector("#current-icon");
   iconElement.setAttribute("alt", response.data.weather[0].description);
   iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+
+  celsiusTemperature= response.data.main.temp;
+  temperatureElement.innerHTML= Math.round(celsiusTemperature);
+ 
+  let fahrTemp = (celsiusTemperature * 9)/5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrTemp);
 }
+ 
+
